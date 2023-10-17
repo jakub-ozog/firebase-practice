376eb607eab5b77c563ee09c48c6b83dcafe506c
jacobharraldson <jharraldson@gmail.com>
2023-10-17 20:26:42 +0200

    feat(firebase/webpack): firebase and webpack config

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import {
+    initializeApp
+} from 'firebase/app'
+import {
+    getFirestore,
+    collection,
+    getDocs,
+    doc
+} from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAiPTOIlHHk4CfsVzlo-MIZcUMU3KLO5lI",
+    authDomain: "jakobov-firebase-practice.firebaseapp.com",
+    projectId: "jakobov-firebase-practice",
+    storageBucket: "jakobov-firebase-practice.appspot.com",
+    messagingSenderId: "430527135702",
+    appId: "1:430527135702:web:a2dfc59429e3aa36f22e77"
+};
+
+//init firebase
+initializeApp(firebaseConfig)
+
+//init services
+const db = getFirestore()
+
+//collection ref
+const colRef = collection(db, 'books')
+
+//get  collection data
+getDocs(colRef)
+    .then((snapshot) => {
+        let books = []
+        snapshot.docs.forEach((doc) => {
+            books.push({
+                ...doc.data(),
+                id: doc.id
+            })
+        })
+
+        console.log(books)
+    })
+    .catch(err => {
+        console.log(err.message)
+    })
+
+// adding documents 
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', () => {
+    e.preventDefault()
+})
+
+// deleting documents 
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', () => {
+    e.preventDefault()
+})
